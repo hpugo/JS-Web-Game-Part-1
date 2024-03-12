d7ced1478acca8ef4f4dcd120d34e64820261efc
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2024-03-12 11:50:10 -0500

    adding work

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(image, src, position, left, bottom, boolean) {
+    let newImg = document.createElement('img');
+    newImg.src = src;
+    newImg.style.position = position;
+    newImg.style.left = left;
+    newImg.style.bottom = bottom;
+    document.body.append(newImg);
+
+    if (boolean === true && image === 'sword') { 
+        newImg.addEventListener('click', function() {
+            newImg.remove();
+        });
+        newImg.style.cursor = 'pointer';
+    }
+}
+
+newImage('sword', 'assets/sword.png', 'fixed', '500px', '405px', true);
+newImage('pineTree', 'assets/pine-tree.png', 'fixed', '450px', '200px');
+newImage('greenCharacter', 'assets/green-character.gif', 'fixed', '100px', '100px');
+newImage('tree', 'assets/tree.png', 'fixed', '200px', '300px');
+newImage('pillar', 'assets/pillar.png', 'fixed', '350px', '100px');
+newImage('crate', 'assets/crate.png', 'fixed', '150px', '200px');
+newImage('well', 'assets/well.png', 'fixed', '500px', '425px');
